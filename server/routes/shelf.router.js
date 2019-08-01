6a52f299db48cc030fd783e460f1bba694fde856
Kyle Olson <kaollie74@gmail.com>
2019-08-01 14:42:27 -0500

    setup server side get, post, delete routes

--- server/routes/shelf.router.js
+++ server/routes/shelf.router.js
@@ -2,11 +2,22 @@ const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
 
+
 /**
  * Get all of the items on the shelf
  */
 router.get('/', (req, res) => {
-    res.sendStatus(200); // For testing only, can be removed
+    const sqlText = `SELECT * FROM "item"`;
+    pool.query(sqlText)
+    .then((response)=>{
+        res.send(response.rows)
+    })
+    .catch((error)=>{
+        console.log('Error getting items from DB', error);
+        res.sendStatus(500)
+        
+    })
+
 });
 
 
@@ -14,6 +25,18 @@ router.get('/', (req, res) => {
  * Add an item for the logged in user to the shelf
  */
 router.post('/', (req, res) => {
+    const sqlText = `INSERT INTO "item" ("description", "image_url", "user_id")
+    VALUES ($1, $2, $3);`;
+    const values = [req.body.description, req.body.image_url, req.body.user_id];
+    pool.query(sqlText, values)
+    .then((response)=>{
+        res.sendStatus(201)
+    })
+    .catch((error)=>{
+        console.log('Error with POSTING to the DB', error);
+        res.sendStatus(500);
+        
+    })
 
 });
 
@@ -22,7 +45,15 @@ router.post('/', (req, res) => {
  * Delete an item if it's something the logged in user added
  */
 router.delete('/:id', (req, res) => {
-
+    const sqlText = `DELETE FROM "item" WHERE user_id = $1;`;
+    pool.query(sqlText, req.params.id)
+    .then((response)=>{
+        res.sendStatus(200);
+    })
+    .catch((error)=>{
+        console.log('Error with DELETING item from DB', error);
+        res.sendStatus(500);
+    })
 });
 
 
